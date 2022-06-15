2721947c7b0d77136033c861e5d44153eec81a41
v-aguiar <v.aguiar.dev@gmail.com>
2022-06-15 13:01:18 -0300

    feat: Add LikeButton component. It toogles between filled and outlined onClick

---
+++ src/components/LikeButton/index.jsx
@@ -0,0 +1,29 @@
+﻿import { useState } from "react";
+
+import { StyledLikeButton, HeartFill, HeartLine } from "./style";
+
+export default function LikeButton({ userId, postId }) {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLiked(!liked);
+    setLikes(liked ? likes - 1 : likes + 1);
+  }
+
+  return (
+    <StyledLikeButton>
+      {liked ? (
+        <>
+          <HeartFill onClick={() => handleClick()} />
+          <p>{likes}</p>
+        </>
+      ) : (
+        <>
+          <HeartLine onClick={() => handleClick()} />
+          <p>{likes}</p>
+        </>
+      )}
+    </StyledLikeButton>
+  );
+}
