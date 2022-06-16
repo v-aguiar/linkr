82deb2031a8c7491e436a2180e55d3daed213f54
v-aguiar <v.aguiar.dev@gmail.com>
2022-06-16 01:07:11 -0300

    chore: Display how many likes a post has beneath the LikeComponent icon

--- src/components/LikeButton/index.jsx
+++ src/components/LikeButton/index.jsx
@@ -1,29 +1,112 @@
-﻿import { useState } from "react";
+﻿import { useState, useEffect } from "react";
+import ReactTooltip from "react-tooltip";
+
+import api from "../../services/api";
 
 import { StyledLikeButton, HeartFill, HeartLine } from "./style";
 
-export default function LikeButton({ userId, postId }) {
+export default function LikeButton({ userId = 6, postId = 1 }) {
+  const [showTooltip, setShowTooltip] = useState(true);
   const [liked, setLiked] = useState(false);
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState({
+    total: "0",
+    user: "0",
+  });
+  // TODO: get users who liked the post before
+  const [likeUsers, setLikeUsers] = useState({
+    first: "",
+    second: "",
+  });
+
+  useEffect(() => {
+    const fetchLikes = async () => {
+      const totalLikes = await api.get(`likes/${postId}/`);
+
+      setLikes({ ...likes, total: totalLikes.data.likes });
+    };
+
+    const fetchUserLike = async () => {
+      const userLike = await api.get(`likes/${postId}/?userId=${userId}`);
+      setLikes({ ...likes, user: userLike.data.likes * 1 });
+      if (userLike.data.likes * 1 === 1) {
+        setLiked(true);
+      }
+    };
+
+    fetchUserLike().catch((err) => {
+      console.error("⚠ Error fetching user like: ", err);
+      return;
+    });
+
+    fetchLikes().catch((err) => {
+      console.error("⚠ Error fetching total likes: ", err);
+      return;
+    });
+  }, []); // eslint-disable-line
 
   function handleClick() {
     setLiked(!liked);
-    setLikes(liked ? likes - 1 : likes + 1);
+    liked
+      ? setLikes({ ...likes, total: likes.total * 1 - 1 })
+      : setLikes({ ...likes, total: likes.total * 1 + 1 });
+  }
+
+  function likeDataTip() {
+    // TODO: refactor function for better performance
+    if (likes.total * 1 === 0) {
+      return "Curtir!";
+    }
+    if (liked && likes.total * 1 === 1) {
+      return `Você curtiu este post`;
+    }
+    if (liked && likes.total * 1 === 2) {
+      return `Você e ${likeUsers.first}`;
+    }
+    if (liked && likes.total * 1 > 2) {
+      return `Você, ${likeUsers.first} e ${likes.total * 1 - 2} pessoas.}`;
+    }
+    if (!liked && likes.total * 1 === 1) {
+      return `${likeUsers.first} curtiu este post`;
+    }
+    if (!liked && likes.total * 1 === 2) {
+      return `${likeUsers.first} e ${likeUsers.second}`;
+    }
+    if (!liked && likes.total * 1 > 2) {
+      return `${likeUsers.first}, ${likeUsers.second} e ${
+        likes.total * 1 - 2
+      } pessoas.}`;
+    }
   }
 
   return (
     <StyledLikeButton>
-      {liked ? (
-        <>
-          <HeartFill onClick={() => handleClick()} />
-          <p>{likes}</p>
-        </>
-      ) : (
-        <>
-          <HeartLine onClick={() => handleClick()} />
-          <p>{likes}</p>
-        </>
+      {showTooltip && (
+        <ReactTooltip
+          className="toolTip"
+          effect="solid"
+          place="bottom"
+          type="light"
+        >
+          <span>{likeDataTip()}</span>
+        </ReactTooltip>
       )}
+      <>
+        {liked ? (
+          <HeartFill onClick={() => handleClick()} />
+        ) : (
+          <HeartLine onClick={() => handleClick()} />
+        )}
+        <p
+          data-tip="ReactTooltip"
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => {
+            setShowTooltip(false);
+            setTimeout(() => setShowTooltip(true), 50);
+          }}
+        >
+          {(likes.total * 1) | []}
+        </p>
+      </>
     </StyledLikeButton>
   );
 }
--- src/components/LikeButton/style.js
+++ src/components/LikeButton/style.js
@@ -3,7 +3,26 @@
 import { RiHeartLine, RiHeartFill } from "react-icons/ri";
 
 export const StyledLikeButton = styled.button`
+  outline: none;
+  border: none;
+
   background-color: #333333;
+
+  .toolTip {
+    color: #505050 !important;
+    font-family: "Lato", sans-serif !important;
+    font-weight: 700 !important;
+    font-size: 0.685rem !important;
+  }
+
+  p {
+    color: #fff;
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 0.685rem;
+    line-height: 0.83rem;
+    text-align: center;
+  }
 `;
 
 export const HeartLine = styled(RiHeartLine)`
