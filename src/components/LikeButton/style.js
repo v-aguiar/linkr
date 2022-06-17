2ef7f290aafba1b3acc5bb74fd00a97bdd64efab
v-aguiar <v.aguiar.dev@gmail.com>
2022-06-17 17:09:56 -0300

    feat: Add LikeButton component. It toogles between filled and outlined onClick

---
+++ src/components/LikeButton/style.js
@@ -0,0 +1,17 @@
+﻿import styled from "styled-components";
+
+import { RiHeartLine, RiHeartFill } from "react-icons/ri";
+
+export const StyledLikeButton = styled.button`
+  background-color: #333333;
+`;
+
+export const HeartLine = styled(RiHeartLine)`
+  color: #fff;
+  font-size: 1.125rem;
+`;
+
+export const HeartFill = styled(RiHeartFill)`
+  color: red;
+  font-size: 1.125rem;
+`;
