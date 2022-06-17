0ee896dcffc95cd6b0f390862f3b555d9946ae72
Hermes Júnior <hermesj.junior@gmail.com>
2022-06-17 16:25:45 -0300

    Modified UserContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,21 +1,5 @@
-﻿import react, {createContext } from "react";
+﻿import { createContext } from "react";
 
-const context = createContext();
+const UserContext = createContext();
 
-export function Provider(props) {
-  // TODO: remove apiUrl from here. Use the function 'api' instead.
-  const apiUrl = "http://localhost:4001";
-  const [refresh, setRefresh] = react.useState(false);
-  
-  return (
-    <context.Provider
-      value={{
-        apiUrl,
-        refresh, setRefresh
-      }}
-    >
-      {props.children}
-    </context.Provider>
-  );
-}
-export const getContext = () => react.useContext(context);
+export default UserContext;
