b394f3973eba00f40010f46d19f710ffb1958f19
Driven Education <marcio.garrido.jr@gmail.com>
2022-06-20 16:09:18 -0300

    feat: timeline with the posts

--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,20 +1,54 @@
-<<<<<<< HEAD
-import Header from "../../components/Header";
-import { Container } from "./style";
-
-export default function TimelinePage() {
-  return (
-    <Container>
-      <Header />
-    </Container>
-=======
+import { useState } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import LinkPreview from "../../components/Link";
 import MainScreen from "../../components/MainScreen/";
+import UserContext from "../../contexts/UserContext";
+import api from "../../services/api";
+import { PostContainer } from "./style";
 
 export default function TimelinePage() {
+
+  const [ posts, setPosts ] = useState([]);
+  const { userInfo } =useContext(UserContext)
+
+  useEffect(()=>{
+    const config = {
+      headers: {
+          Authorization: `Bearer ${userInfo.token}`
+      }
+  }
+    const promisse = api.get("timeline", config)
+    promisse.then((res)=>{
+      const {data} = res
+      setPosts(data)
+    })
+    promisse.catch((error) => {
+      alert(error.response.data);
+    });
+  },[]
+  )
+
   return (
     <MainScreen>
-      <h1>timeline</h1>
+      {posts.map((post)=>{
+        const { username, text, imgUrl, url} = post
+        return(
+          <>
+            <PostContainer>
+              <section>
+                <img className="user" src={imgUrl} alt="" />
+                {/* {handleLikes()} */}
+              </section>
+              <div className="post-body">
+                <h2>{username}</h2>
+                <p>{text}</p>
+                <LinkPreview metaData={post} />
+              </div>
+		        </PostContainer>
+          </>
+        )
+      })}
     </MainScreen>
->>>>>>> dd87482fc1169fd9fbe8ac8628e196b1b0665f29
   );
 }
