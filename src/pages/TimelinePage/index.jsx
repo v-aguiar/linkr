10ff43cab56999cfa2d82e57ee218c7dd273dd21
Driven Education <marcio.garrido.jr@gmail.com>
2022-06-20 13:21:13 -0300

    merge

--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Header from "../../components/Header";
 import { Container } from "./style";
 
@@ -6,5 +7,14 @@ export default function TimelinePage() {
     <Container>
       <Header />
     </Container>
+=======
+import MainScreen from "../../components/MainScreen/";
+
+export default function TimelinePage() {
+  return (
+    <MainScreen>
+      <h1>timeline</h1>
+    </MainScreen>
+>>>>>>> dd87482fc1169fd9fbe8ac8628e196b1b0665f29
   );
 }
